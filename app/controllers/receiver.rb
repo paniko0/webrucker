0ca136306179336331f578a98a7c7a6cb2c1c2f9
Danillo Souza <danillo.fs@gmail.com>
2013-01-09 14:31:04 -0200

    header te odeio

--- app/controllers/receiver.rb
+++ app/controllers/receiver.rb
@@ -7,14 +7,12 @@ Webrucker.controllers :receiver do
 
   get :index, :with => :login do
     @users = User.find_by(login: params[:login])
-    logger.info @users.response[0]
-    @authorization_header = @users["header"].nil? ? "NoAuth" : @users["header"]["authorization"]
     render 'receiver/list'
   end
 
   post :new, :with => :login do
     user = User.find_or_initialize_by(login: params[:login])
-    user.push(:response, :json => request.body.read, :datetime => Time.now, :header => request_headers)
+    user.push(:response, :json => request.body.read.force_encoding('UTF-8'), :datetime => Time.now)
     user.save!
   end
 
