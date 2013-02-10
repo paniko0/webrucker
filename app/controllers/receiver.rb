edd8ba4a63e234b59b7cf61b7fe070271ed5d2f1
Danillo Souza <danillo.fs@gmail.com>
2013-02-09 22:10:03 -0200

    correcting http method =P

--- app/controllers/receiver.rb
+++ app/controllers/receiver.rb
@@ -17,6 +17,12 @@ Webrucker.controllers :receiver do
     end
   end
 
+  post :with => :login do
+    user = User.find_or_initialize_by(login: params[:login])
+    user.push(:response, :json => request.body.read.force_encoding('UTF-8'), :received_date => Time.now, :header => request_headers)
+    user.save!
+  end
+
   post :new, :with => :login do
     user = User.find_or_initialize_by(login: params[:login])
     user.push(:response, :json => request.body.read.force_encoding('UTF-8'), :received_date => Time.now, :header => request_headers)
